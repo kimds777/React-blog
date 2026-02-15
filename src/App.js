fc035cf440d63a6eea662515aed2f1ea96372ae3
Daseul Kim <tina27849@gmail.com>
2026-02-15 14:31:39 +0900

    feat: React JSX 문법 기초 활용 style 적용

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // 실무에선 DB연결해서 사용
+  let postTitle = '포스트 제목'; 
+
+  // html id와 class에도 변수사용 가능
+  let htmlId = 'postTitle';
+  let htmlClass = 'black-nav';
+
+  // html에 style 적용시 중괄호 사용해서 object형태로 넣어줘야한다.
+  // font-size와 같은 css는 카멜케이스로 수정해서 사용해야한다.
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={htmlClass}>
+        <h4 style={ {color:'yellow', fontSize:'16px'} }>React-blog</h4>
+      </div>
+      <h4 id={htmlId}>{postTitle}</h4>
     </div>
   );
 }
